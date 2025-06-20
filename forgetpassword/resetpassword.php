d61690046a49189e11b063743af0787898db14dc
mohamadalnabhan <mhamadnabhan6@gmail.com>
2025-06-20 22:44:29 +0300

    forget password stuff

--- forgetpassword/resetpassword.php
+++ forgetpassword/resetpassword.php
@@ -2,7 +2,7 @@
 include "../connect.php";
 
 $email = filterRequest ("email");
-$password = filterRequest ("password");
+$password = sha1($_POST['password']) ;
 $data = array("users_password" => $password) ; 
 
 updateData("users" , $data ,"users_email = '$email' ");
