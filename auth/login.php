21de932592bcb6d74dc8ea4637d5491f32e6a68d
mohamadalnabhan <mhamadnabhan6@gmail.com>
2025-06-20 14:38:02 +0300

    login addd

--- auth/login.php
+++ auth/login.php
@@ -5,12 +5,9 @@ include "../mail/mailer.php" ;
 
 $email = filterRequest("email");
 $password = sha1("password") ;
-
-
 $stmt =$con-> prepare("SELECT * FROM users WHERE `users_email` = ? OR `users_password` = ?");
 $stmt -> execute(array($email , $password));
 $count = $stmt->rowCount();
+result($count);
 
 
-
-?>
