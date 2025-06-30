d55f94fcd7d8569651376bbcc946e26ae3c66c9e
mohamadalnabhan <mhamadnabhan6@gmail.com>
2025-06-30 08:44:06 +0200

    home php

--- home.php
+++ home.php
@@ -6,7 +6,5 @@ $getDataArray = array();
 $categories = getAllData("categories", null, null, false);
 
 $getDataArray['categories'] = $categories;
-
-header("Content-Type: application/json"); // optional, makes it return JSON properly
 echo json_encode($getDataArray);
 ?>
