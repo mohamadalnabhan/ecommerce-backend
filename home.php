4c1c409fb2f7061fda52551e0fce5ec7e9cca9de
mohamadalnabhan <mhamadnabhan6@gmail.com>
2025-06-30 08:35:48 +0200

    home php

--- home.php
+++ home.php
@@ -10,6 +10,6 @@ $categories = getAllData("categories" , null , null , false);
 $getDataArray['categories'] = $categories ;  // ['categories is a key']
 
 
-return json_encode($getDataArray);
+echo json_encode($getDataArray);
 
 ?>
