b45da9de60b6d911cb6e4e9bd3d830bd6c1415d4
mohamadalnabhan <mhamadnabhan6@gmail.com>
2025-07-01 11:49:44 +0200

    items backend home pge

--- home.php
+++ home.php
@@ -7,4 +7,9 @@ $categories = getAllData("categories", null, null, false);
 
 $getDataArray['categories'] = $categories;
 echo json_encode($getDataArray);
+
+$items = getAllData("items" , "`items_discount` != 0 " , null , false);
+$getDataArray['items'] = $items;
+echo json_encode($getDataArray);
+
 ?>
