8cd5e0cdd8f2535453754afe7863ae328fbe5b58
mohamadalnabhan <mhamadnabhan6@gmail.com>
2025-07-08 18:34:33 +0300

    items php

--- items/items.php
+++ items/items.php
@@ -1,13 +1,16 @@
 <?php
 ob_start();
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 include "../connect.php";
 
 $categories_id = filterRequest("id");
+
+// Log incoming ID
+file_put_contents("debug_id_log.txt", "ID = " . $categories_id);
+
+if (empty($categories_id)) {
+    echo json_encode(["status" => "failure", "message" => "Missing category ID"]);
+    exit;
+}
 
 getAllData("items1view", "categories_id = ?", [$categories_id]);
 
