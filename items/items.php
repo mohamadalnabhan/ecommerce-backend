afa8b0f62c22430f3c89cd3541ddc1e7ccfb7002
mohamadalnabhan <mhamadnabhan6@gmail.com>
2025-07-08 18:21:00 +0300

    items php

--- items/items.php
+++ items/items.php
@@ -1,5 +1,5 @@
 <?php
-ob_start(); // ✅ buffer output
+ob_start();
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -7,9 +7,8 @@ error_reporting(E_ALL);
 
 include "../connect.php";
 
-
 $categories_id = filterRequest("id");
 
-getAllData("items1view", "categories_id = $categories_id");
+getAllData("items1view", "categories_id = ?", [$categories_id]);
 
-ob_end_flush(); // ✅ flush output
+ob_end_flush();
