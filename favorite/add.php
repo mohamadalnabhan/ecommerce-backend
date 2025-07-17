94f5aed8cbb0eb752735b5e7a24bca387fe97a3d
mohamadalnabhan <mhamadnabhan6@gmail.com>
2025-07-17 13:44:52 +0300

    add php edit

--- favorite/add.php
+++ favorite/add.php
@@ -2,7 +2,7 @@
 include "../connect.php";
 
 $userid = filterRequest("userid");
-$itemsid = filterRequest("itemid"); // ✅ Make sure this matches what's sent from Flutter
+$itemsid = filterRequest("itemid");
 
 $data = array(
     "favorite_usersid" => $userid,
@@ -13,6 +13,9 @@ $result = insertData("favorite", $data);
 
 if ($result > 0) {
     echo json_encode(["status" => "success"]);
+    exit(); // ✅ make sure you EXIT
 } else {
     echo json_encode(["status" => "failure"]);
+    exit(); // ✅ make sure you EXIT
 }
+?>
