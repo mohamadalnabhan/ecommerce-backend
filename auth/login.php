209fcf277654816ce776b16b089376dc6fc55dfd
mohamadalnabhan <mhamadnabhan6@gmail.com>
2025-07-23 12:52:31 +0300

    login  php

--- auth/login.php
+++ auth/login.php
@@ -4,10 +4,9 @@ include "../mail/mailer.php" ;
 
 
 $email = filterRequest("email");
-$password = filterRequest("password");
+$password = sha1($_POST['password']);
 // $stmt =$con-> prepare("SELECT * FROM users WHERE `users_email` = ? AND `users_password` = ? AND users_approved = 1");
 // $stmt -> execute(array($email , $password));
 // $count = $stmt->rowCount();
 // result($count);
-
- getData("users" ,"`users_email` = ? AND `users_password` = ?" , array($email ,$password));
+getData("users", "`users_email` = ? AND `users_password` = ?", array($email, $password));
