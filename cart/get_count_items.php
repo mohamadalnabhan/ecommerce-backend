2b14bfb23f59ba6e74a4da508bdb6aaa0fb172f5
mohamadalnabhan <mhamadnabhan6@gmail.com>
2025-07-27 18:44:08 +0300

    Add delete cart  php

---
+++ cart/get_count_items.php
@@ -0,0 +1,27 @@
+<?php 
+
+ include "../connect.php" ; 
+
+ $userid = filterRequest("userid") ; 
+ $itemid = filterRequest("itemid") ; 
+
+ $stmt = $con->prepare("SELECT COUNT(cart.cart_id) as countitems  FROM `cart` WHERE cart_userid = $userid AND cart_itemid  =  $itemid");
+ $stmt->execute() ; 
+
+ $count = $stmt->rowCount() ; 
+
+ $data = $stmt->fetchColumn() ; 
+ 
+
+  if ($count > 0 ){
+    
+    echo json_encode(array("status" => "success" , "data" => $data)) ; 
+
+  } else {
+
+    echo json_encode(array("status" => "success" , "data" => "0")) ; 
+
+  }
+
+
+?>
