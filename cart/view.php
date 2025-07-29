f5ec235d631e0db6e95c751690bd173a437ba250
mohamadalnabhan <mhamadnabhan6@gmail.com>
2025-07-29 11:26:12 +0300

    view cart  php

--- cart/view.php
+++ cart/view.php
@@ -4,9 +4,23 @@ include "../connect.php";
 
 // Assuming filterRequest function is defined in connect.php or an included utility
 $userid = filterRequest("userid");
-$itemid = filterRequest("itemid"); // Using 'itemid' consistently
 
 
+$data = getAllData("cartview" , "cart_userid = $userid" , null , false);
+
+$stmt = $con->prepare("SELECT SUM(totalPrice) as totalItemsPrice , SUM(itemCount) as totalItemCount FROM `cartview`
+
+WHERE cart_userid
+GROUP BY cartview.cart_userid = $userid");
+$stmt -> execute();
+
+$dataCountAndPrice = $stmt->fetch(PDO::FETCH_ASSOC) ; 
+
+echo json_encode(array(
+    "status" => "success" ,
+    "datacart" => $data,
+    "countAndPrice" =>$dataCountAndPrice
+));
 
 
 ?>
