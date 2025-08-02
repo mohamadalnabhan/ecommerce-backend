73a53006ca569054d3447e73528ea4312cc437cf
mohamadalnabhan <mhamadnabhan6@gmail.com>
2025-08-02 11:38:08 +0300

    SEARCH  php

--- items/search.php
+++ items/search.php
@@ -3,7 +3,8 @@ include "../connect.php" ;
 
 $search = filterRequest("search");
 
-getAllData("items" , "items_name_en LIKE `%$search%` OR items_name_ar LIKE `%$search%` "); 
+
+getAllData("items", "items_name_en LIKE '%$search%' OR items_name_ar LIKE '%$search%'");
 
 
 ?>
