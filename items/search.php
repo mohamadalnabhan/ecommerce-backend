0226029e140fe2cbf2d596f0e23db4f824ce733b
mohamadalnabhan <mhamadnabhan6@gmail.com>
2025-08-07 12:04:18 +0300

    SEARCH  php

--- items/search.php
+++ items/search.php
@@ -4,7 +4,7 @@ include "../connect.php" ;
 $search = filterRequest("search");
 
 
-getAllData("items", "items_name_en LIKE '%$search%' OR items_name_ar LIKE '%$search%'");
+getAllData("items2view", "items_name_en LIKE '%$search%' OR items_name_ar LIKE '%$search%'");
 
 
 ?>
