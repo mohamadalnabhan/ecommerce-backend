0f4bcdef10e7f039debb884f35c69cfa03dca61f
mohamadalnabhan <168739987+mohamadalnabhan@users.noreply.github.com>
2026-02-10 23:22:26 +0200

    Update delete.php

--- cart/delete.php
+++ cart/delete.php
@@ -1,14 +1,11 @@
 <?php
 include "../connect.php";
 
-// Use consistent parameter names with add.php
 $userid = filterRequest("userid");
 $itemid = filterRequest("itemid");
 
-// Fix 1: Use correct column name 'cart_userid' not 'cart_usersid'
-// Fix 2: Delete directly without subquery
-$conditions = "cart_userid = ? AND cart_itemid = ? AND cart_orders = 0";
-$params = array($userid, $itemid);
+// Build WHERE condition directly
+$where = "cart_userid = $userid AND cart_itemid = $itemid AND cart_orders = 0";
 
-// If you have deleteData function
-deleteData("cart", $conditions, $params);
+// Call deleteData WITHOUT params
+deleteData("cart", $where);
